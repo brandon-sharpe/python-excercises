be28d2fabb59bc6f53c4f250a016004ca4e49fc5
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-07-08 11:46:22 -0500

    Completed python_introduction_excercise.py

---
+++ python_introduction_exercises.py
@@ -0,0 +1,12 @@
+# Create a hello world program. Create a text file named 
+# python_introduction_exercises.py and write a program 
+# that prints "Hello, World!" to the console. 
+# Run this program from the command line
+print("Hello, World!")
+# Inside of your hello world program, create a variable named greeting
+# that contains the message that you will print to the console. 
+# Run your script interactively
+# (with python -i python_introduction_exercises.py) 
+# and view the contents of the greeting variable.
+greeting = "Hello my name is Brandon and this is my first line of code for the Germain Cohort."
+
