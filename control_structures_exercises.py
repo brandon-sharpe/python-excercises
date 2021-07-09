79eedf9da3a3f5f0944b0ccc2a6a6b5bd570be73
brandon-sharpe <brandon.robert.sharpe@gmail.com>
2021-07-09 17:06:32 -0500

    fixed

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -9,7 +9,6 @@ else:
 
 ##1.B) prompt the user for a day of the week,
 # print out whether the day is a weekday or a weekend
-
 is_it_the_weekend = input(f"# 1.B) What day of the week is it? ")
 if is_it_the_weekend.lower() == "saturday":
     print("    IT'S THE WEEKEND!")
@@ -19,7 +18,6 @@ elif is_it_the_weekend.lower() == "sunday":
 
 else:
     print("   Damn today is a weekday!")
-
 # # 1.C) create variables and make up values for
 # the number of hours worked in one week
 # the hourly rate
@@ -31,6 +29,6 @@ hours_per_week= 60
 hourly_rate =45
 
 if hours_per_week > 40:
-    print((hours_per_week - 40)* hourly_rate*1.5 + 40*hourly_rate)
+    print(f"You made ${(hours_per_week - 40)* hourly_rate*1.5 + 40*hourly_rate} for working {hours_per_week} hours this week, at the rate of ${hourly_rate} an hour")
 else:
     print(hours_per_week*hourly_rate)
